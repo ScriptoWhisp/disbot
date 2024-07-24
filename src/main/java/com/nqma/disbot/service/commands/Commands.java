ffae9223fa77c5d700eba71e35f68be099ac09fc
ScriptoWhisp <daniel.tjulinov@gmail.com>
2024-07-24 17:05:00 +0300

    Commands enum improve

--- src/main/java/com/nqma/disbot/service/commands/Commands.java
+++ src/main/java/com/nqma/disbot/service/commands/Commands.java
@@ -1,20 +1,11 @@
 package com.nqma.disbot.service.commands;
 
 public enum Commands {
-    PLAY("play"),
-    PAUSE("pause"),
-    STOP("stop"),
-    NEXT("next"),
-    PREVIOUS("previous"),
-    PLIST("plist");
-
-    private final String name;
-
-    Commands(String name) {
-        this.name = name;
-    }
-
-    public String toString() {
-        return name;
-    }
+    PLAY,
+    PAUSE,
+    STOP,
+    NEXT,
+    PREVIOUS,
+    PLIST,
+    STATUS
 }
