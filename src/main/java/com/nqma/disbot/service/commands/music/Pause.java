baee8cc0ab4a61e7425afed8a58db0c0070457d6
ScriptoWhisp <daniel.tjulinov@gmail.com>
2024-07-24 17:01:56 +0300

    pause command

--- src/main/java/com/nqma/disbot/service/commands/music/Pause.java
+++ src/main/java/com/nqma/disbot/service/commands/music/Pause.java
@@ -1,8 +1,12 @@
 package com.nqma.disbot.service.commands.music;
 
 import com.nqma.disbot.service.commands.Commands;
+import com.nqma.disbot.service.commands.Message;
 import com.nqma.disbot.service.commands.SlashCommand;
+import com.nqma.disbot.service.player.GuildQueue;
+import com.nqma.disbot.service.responsers.MessageSender;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Member;
 import reactor.core.publisher.Mono;
 
@@ -14,6 +18,27 @@ public class Pause implements SlashCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        return null;
+
+        System.out.println("Pause command");
+
+        Member member = event.getInteraction().getMember().orElse(null);
+
+        VoiceState voiceState = member.getVoiceState().block();
+        if (voiceState == null) {
+            return MessageSender.replyTo(event, Message.NOT_IN_VOICE_CHANNEL);
+        }
+
+        GuildQueue guildQueue = GuildQueue.getGuildQueue(voiceState);
+        if (guildQueue == null) {
+            return MessageSender.replyTo(event, Message.NO_SONGS_PLAYING);
+        }
+
+        if (voiceState.getChannel().block().getId().asLong() != guildQueue.getChannel().getId().asLong()) {
+            return MessageSender.replyTo(event, Message.NOT_IN_VOICE_CHANNEL);
+        }
+
+        System.out.println("pausing");
+        if (guildQueue.pause()) return MessageSender.replyTo(event, Message.PAUSED);
+        return MessageSender.replyTo(event, Message.RESUMED);
     }
 }
